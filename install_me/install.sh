075e0ace55fbc24f8d62d626c66b3bb9b17d91f6
Eric Salomaki <eric.salomaki@gmail.com>
2026-01-14 12:01:35 -0500

    Update install.sh

--- install_me/install.sh
+++ install_me/install.sh
@@ -5,6 +5,6 @@ if [ ! -f "$HOME/.bashrc.bak" ]; then cp ~/.bashrc ~/.bashrc.bak; fi
 # Create a user-friendly alias to shut off virtual environments 
 grep -qxF 'alias quit_workflow="deactivate && bash"' ~/.bashrc || echo 'alias quit_workflow="deactivate && bash"' >> ~/.bashrc; 
 # Load needed modules 
-module load python/3.9.16s-x3wdtvt openjdk/11.0.23_9-7cjq
+module load python/3.9.16s-x3wdtvt openjdk/17.0.11_9-7sqr
 # Run Python script 
 python3 $HOME/workflows_on_OSCAR/workflows.py 
